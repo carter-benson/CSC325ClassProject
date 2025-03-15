6e20946a01777f16ac65b8d24c0a0c239e38e782
Carter Benson <145940670+carter-benson@users.noreply.github.com>
2025-03-15 21:20:15 +0000

    added mui template configurations

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -8,6 +8,17 @@ import NextLink from 'next/link';
 import ProTip from '@/components/ProTip';
 import Copyright from '@/components/Copyright';
 
+import Divider from '@mui/material/Divider';
+import LogoCollection from '@/components/LogoCollection';
+import Features from '@/components/Features';
+import Hero from '@/components/Hero';
+import AppAppBar from '@/components/AppAppBar';
+import Testimonials from '@/components/Testimonials';
+import Highlights from '@/components/Highlights';
+import Pricing from '@/components/Pricing';
+import FAQ from '@/components/FAQ';
+import Footer from '@/components/Footer';
+
 export default function Home() {
   return (
     <Container maxWidth="lg">
@@ -26,36 +37,54 @@ export default function Home() {
         <Link href="/about" color="secondary" component={NextLink}>
           Go to the about page
         </Link>
+
+        <AppAppBar />
+        <Hero />
+        <div>
+          <LogoCollection />
+          <Features />
+          <Divider />
+          <Testimonials />
+          <Divider />
+          <Highlights />
+          <Divider />
+          <Pricing />
+          <Divider />
+          <FAQ />
+          <Divider />
+          <Footer />
+        </div>
+
         <ProTip />
         <Copyright />
       </Box>
     </Container>
   );
 }
- //import container 
- //import box
- //import hero
- //import LogoCollection from '@/components/LogoCollection';
- //import Features from '@/components/Features';
- //import Divider
- //
- //export default function Home
- // return (
- //   <Container maxWidth="lg">
- //   <Box
- //     sx={{
- //       
- //     
- //       Divider
- //       Testimonials
- //       Divider
- //       Highlights
- //       Divider
- //       Pricing
- //       Divider
- //       FAQ
- //       Divider
- //       Footer
- //   </  div>
- // </Box>
- //</Container>
+//import container 
+//import box
+//import hero
+//import LogoCollection from '@/components/LogoCollection';
+//import Features from '@/components/Features';
+//import Divider
+//
+//export default function Home
+// return (
+//   <Container maxWidth="lg">
+//   <Box
+//     sx={{
+//
+//
+//       Divider
+//       Testimonials
+//       Divider
+//       Highlights
+//       Divider
+//       Pricing
+//       Divider
+//       FAQ
+//       Divider
+//       Footer
+//   </  div>
+// </Box>
+//</Container>
--- website/src/components/AppAppBar.tsx
+++ website/src/components/AppAppBar.tsx
@@ -12,8 +12,9 @@ import MenuItem from '@mui/material/MenuItem';
 import Drawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import ColorModeIconDropdown from '../../shared-theme/ColorModeIconDropdown';
+import ColorModeIconDropdown from '../shared-theme/ColorModeIconDropdown';
 import Sitemark from './SitemarkIcon';
+import type {} from '@mui/material/themeCssVarsAugmentation';
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   display: 'flex',
